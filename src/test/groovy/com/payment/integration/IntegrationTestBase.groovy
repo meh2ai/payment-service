ef84b0f406c9e03257f342e36508a9c76c9bf39d
meh2ai <meh2ai@dev.com>
2025-12-01 19:41:56 +0100

    test: add integration test base with Testcontainers

---
+++ src/test/groovy/com/payment/integration/IntegrationTestBase.groovy
@@ -0,0 +1,36 @@
+package com.payment.integration
+
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.kafka.ConfluentKafkaContainer
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.spock.Testcontainers
+import org.testcontainers.utility.DockerImageName
+import spock.lang.Specification
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+abstract class IntegrationTestBase extends Specification {
+
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:18")
+            .withDatabaseName("payments_test")
+            .withUsername("test")
+            .withPassword("test")
+
+    static ConfluentKafkaContainer kafka = new ConfluentKafkaContainer(
+            DockerImageName.parse("confluentinc/cp-kafka:7.9.5"))
+
+    static {
+        postgres.start()
+        kafka.start()
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl)
+        registry.add("spring.datasource.username", postgres::getUsername)
+        registry.add("spring.datasource.password", postgres::getPassword)
+        registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers)
+    }
+}
